efc233e7d7e15065000e51f8d94bae400e0661fb
kkumar2_be16@thapar.edu <kkumar2_be16@thapar.edu>
2020-10-26 01:30:37 +0530

    [Kirti]Added:implemented premium rides

--- src/main/java/com/cabinvoicegenerator/Ride.java
+++ src/main/java/com/cabinvoicegenerator/Ride.java
@@ -1,11 +1,13 @@
 package com.cabinvoicegenerator;
 
 public class Ride {
+	public CabRide cabRide;
 	public double distance;
 	public int time;
-	
-	public Ride(double distance,int time) {
-		this.distance=distance;
-		this.time=time;
+
+	public Ride(double distance, int time, CabRide cabRide) {
+		this.distance = distance;
+		this.time = time;
+		this.cabRide = cabRide;
 	}
 }
--- src/main/java/com/cabinvoicegenerator/RideRepository.java
+++ src/main/java/com/cabinvoicegenerator/RideRepository.java
@@ -6,21 +6,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class RideRepository {
-	Map<String, ArrayList<Ride>> userToRides = null;
+	Map<String, ArrayList<Ride>> userRides = null;
 
 	public RideRepository() {
-		userToRides = new HashMap<>();
+		userRides = new HashMap<>();
 	}
 
 	public void addRides(String userId, Ride[] rides) {
-		ArrayList<Ride> rideList = this.userToRides.get(userId);
+		ArrayList<Ride> rideList = this.userRides.get(userId);
 		if (rideList == null) {
-			rideList = new ArrayList<>(Arrays.asList(rides));	
+			rideList = new ArrayList<>(Arrays.asList(rides));
+			this.userRides.put(userId, rideList);
 		}
-		this.userToRides.put(userId, rideList);
 	}
 
-	public ArrayList<Ride> getRides(String userId) {
-		return this.userToRides.get(userId);
+	public Ride[] getRides(String userId) {
+		return this.userRides.get(userId).toArray(new Ride[0]);
 	}
 }
