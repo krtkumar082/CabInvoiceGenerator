471313e705e948804656f96f15bdd9f4957694e2
kkumar2_be16@thapar.edu <kkumar2_be16@thapar.edu>
2020-10-26 01:01:20 +0530

    [Kirti]Added:calculate fare with given distance and time

---
+++ src/main/java/com/cabinvoicegenerator/InvoiceGenerator.java
@@ -0,0 +1,14 @@
+package com.cabinvoicegenerator;
+
+public class InvoiceGenerator {
+	private static final double COST_PER_KILOMETER = 10;
+	private static final int COST_PER_TIME = 1;
+	private static final double MIN_FARE = 5;
+	
+	public double calculateFare(double distance,int time) {
+		double fare = distance * COST_PER_KILOMETER + time * COST_PER_TIME;
+		if (fare < MIN_FARE)
+			return MIN_FARE;
+		return fare;
+	}
+}
